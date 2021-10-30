6324705936d4146f5a238d0604f3b71b1e9ec6a7
skyblue-hux <86983325+skyblue-hux@users.noreply.github.com>
2021-10-30 00:49:30 -0700

    Update chat.php

--- chat.php
+++ chat.php
@@ -14,7 +14,7 @@
   echo '<script>localStorage.setItem("User", "' . $user_name . '")</script>';
   if ($_GET['message']) {
   file_put_contents('chats.txt', '<p class="' . $user_name . ' msg"><span style="font-weight:bold">' . $user_name . ': </span>' . htmlentities($_GET['message']) . '</p>' . PHP_EOL, FILE_APPEND | LOCK_EX);
-  echo '<script>window.location.href = "https://serverchat-123.herokuapp.com//Chat/chat.php?user_name=' . $user_name . '&message=&submit=Submit+Query";</script>';}
+  echo '<script>window.location.href = "https://serverchat-123.herokuapp.com/chat.php?user_name=' . $user_name . '&message=&submit=Submit+Query";</script>';}
 ?>
 
 <div id="messages"></div>
